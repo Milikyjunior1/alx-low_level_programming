6770f8752347cc9cb30d9a58024f334399c7efe6
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-16 14:50:09 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry block
+ * Retuen: Always 0 (success)
+ */
+int main(void)
+{
+	printf("size of a char : %i byte(s)\n",(int) sizeof(char));
+	printf("size of a  int : %i byte(s)\n",(int) sizeof(int));
+	printf("size of a long int : %i byte(s)\n",(int) sizeof(long int ));
+	printf("size of a long long int: %i byte(s)\n",(int) sizeof(long long int ));
+	printf("size of a float : %i byte(s)\n",(int) sizeof(float));
+	return (0);
+}
