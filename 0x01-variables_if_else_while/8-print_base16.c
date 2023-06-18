ccefbc2ba186005b4868c5077f33ca275279c6c6
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-18 12:40:38 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char let;
+
+	for (num = 0; num = 10; num++)
+		putchar(num);
+	for (let = 'a'; let <= 'f'; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
