7cbc9605f2461db5871312e209cb30f3610e34df
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-18 05:34:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'z'; let <= 'a'; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
