78410dba0e5f185703a5bab77fabc3d94cbfbd24
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-18 05:27:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always block
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
