908993b28237b8be89710c31718848cd63b9d79a
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-18 05:12:58 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+		printf("%i\n", num);
+	return (0);
+}
