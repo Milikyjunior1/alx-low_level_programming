0b0c67f7992aa3acbc04b30ce03e36748b2a80a1
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-18 03:51:50 -0700

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = "a" ; let <= "z" ; let++)
+		putchar(let);
+	putchar("\n");
+	return (0);
+}
