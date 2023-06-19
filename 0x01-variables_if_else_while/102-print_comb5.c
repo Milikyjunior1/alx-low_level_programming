cd751c886be93d883fadf34e5f967b17a3898cbe
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-19 14:12:38 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int num, num1;
 
-	for (num = 0; num < 98; num++)
+	for (num = 0; num <= 98; num++)
 	{
-		for (num1 = num + 1; num1 < 99; num1++)
+		for (num1 = num + 1; num1 <= 99; num1++)
 		{
 			putchar((num / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num / 10) + '0');
 			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			if (num == 98 && num1 == 99)
 				continue;
 			putchar(',');
