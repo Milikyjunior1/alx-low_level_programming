e5151704082dec23944d5ce59ba4e36cae29240e
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-19 10:43:39 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,8 +6,10 @@
 int main(void)
 {
 	int num, num1;
-	
-	for (num1 = num + 1; num1 < 10; num1++)
+
+	for (num = 0; num < 9; num++)
+	{
+		for (num1 = num + 1; num1 < 10; num1++)
 		{
 			putchar((num % 10) + '0');
 			putchar((num1 % 10) + '0');
