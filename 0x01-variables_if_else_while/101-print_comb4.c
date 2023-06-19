e8569aa9999756db307f1e3a78f0f5b8fbacff2c
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-19 11:31:33 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Entry block
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num, num1, num2;
+
+	for (num = 0; num < 8; num++)
+	{
+		for (num1 = num + 1; num1 < 9; num1++)
+		{
+			for (num2 = num1 + 1; num2 < 10; num2++)
+			{
+				putchar((num % 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num == 7 && num1 == 8 && num2 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
