9b809985b67481393ac430a68d3cb843e30c2787
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-22 07:10:11 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - a function that print numbers
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n);
+	_putchar('\n');
+}
