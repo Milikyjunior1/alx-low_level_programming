b96f373fc3eef8d7748346da29df91a60d90b07e
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-22 06:47:18 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - to multiply two numbers
+ * @a: is multiply with b
+ * @b: is multiply with a
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
