e407ef77b96cbde10a3adb46fba65dae1540d5a9
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-26 14:25:46 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - for pronting half of character
+ * @str: the half
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int half, i, jj;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else if (i % 2 == 1)
+	{
+		for (j = str[(i - 1) / 2]; j < i - 1; j++)
+		{
+			_putchar(str[j]);
+		}
+
+	}
+	_putchar('\n');
+}
