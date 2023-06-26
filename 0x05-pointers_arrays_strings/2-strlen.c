b1c00f5047d8df5f50bbae6dd43f659cb8dedce8
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-26 11:30:06 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - foe conunting number of character
+ * @s: the character
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i  = 0;
+	while (i != EOF)
+	{
+		i++;
+	}
+	return (i);
+}
