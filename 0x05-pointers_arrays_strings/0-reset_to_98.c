ee1285f45ccf722e1c5d80aecfff88544b6846d2
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-06-26 10:16:36 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry block
- * Return: Always 0
  * reset_to_98 - reset to 98
+ * @n: set to 98
+ *
+ * Return: Always 0
  */
-void reset_to_98(int n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
