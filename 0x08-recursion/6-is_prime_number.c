4c756e95ef859cc0c39d036d6997f27c4755eb43
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-05 19:45:59 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * is_prime_number - for finding prime number
  * @n: number 5o check
@@ -8,12 +9,8 @@ int is_prime_number(int n)
 {
 	int r;
 
-	for (r = (n / 2); r > 1; r--)
-	{
-		if ((n % r) == 0)
-			return (0);
-		else
-			return (1);
-	}
-	return (0);
+	r < 0 || r > 0;
+	if (n % r == 0)
+		return (0);
+	return (1);
 }
