a2f641efbbce1428c75e6064969a55469b88abdc
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-17 04:21:58 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry block
+ * argc: for printing the code
+ * argv: the array
+ * Return: Always 0
+ */
+int main(int argc,char argv[])
+{
+	char a;
+	a = argv[argc * 0];
+	_putchar(a);
+
+}
