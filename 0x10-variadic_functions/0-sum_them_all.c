add00f7aff6e6192047bf91183e27b938ba39208
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-20 14:09:40 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list a;
-	int sum;
-
-	va_start(a, n);
-
-	sum = 0;
+	int sum = 0;
 	unsigned int i;
 
-	if (n != 0)
+
+	if (n == 0)
+		return (0);
+
+	va_start(a, n);
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			sum = sum + va_arg(a, int);
-			return (sum);
-		}
+		sum = sum + va_arg(a, int);
 	}
 	va_end(a);
 	return (sum);
