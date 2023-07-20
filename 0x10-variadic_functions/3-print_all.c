12363f96dab8804638c8a9eaea32247495d92b5b
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-20 15:36:31 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_all - for printing all
+ * @format: to specify format
+ * Return: Always 0
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	unsimed int in;
+	float fl;
+
+}
+
