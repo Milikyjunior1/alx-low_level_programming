9c027a874e45a0266d0c3d11cc7d43a777c16146
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-27 15:59:57 -0700

    0-print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LIST
-#define LINT
+#define LIST
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,7 +13,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *next;;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
