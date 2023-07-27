d764890015916707ec56303002cb574878e3be6a
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-07-27 15:36:28 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - for printing list
+ * @h: lenght of the array str
+ * Return: Always 0
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if ((h->str) == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%u] (%s)\n", h->len, h->str);
+			h = h->next;
+		}
+		i++;
+	}
+	if (i)
+	{
+		return (i);
+	}
+	else
+		return (0);
+}
+
