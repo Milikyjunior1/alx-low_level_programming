a67c89dc37985fdf7b6809c4307e414650a8adce
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-01 13:00:24 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * printlistint - for printing link list
+ * @h: the index of node
+ * Return: Alwayd 0
+ */
+size_t printlistint(const listint_t *h)
+{
+	while (h)
+	{
+		size_t p;
+
+		for (p = 0; h->next != NULL; p++)
+		{
+			printf("%d\n", h->n);
+		}
+		return (p);
+	}
+	return (0);
+}
