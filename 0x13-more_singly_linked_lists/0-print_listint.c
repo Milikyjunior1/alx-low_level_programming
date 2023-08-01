20fcdc0c9b1c48073f5df525a7fdbda684d6822e
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-01 13:15:06 -0700

    1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,12 @@
  */
 size_t printlistint(const listint_t *h)
 {
+	size_t p;
+
 	while (h)
 	{
-		size_t p;
-
-		for (p = 0; h->next != NULL; p++)
-		{
-			printf("%d\n", h->n);
-			return (p);
-		}
+		p++;
+		printf("%d\n", h->n);
 	}
+	return (p);
 }
