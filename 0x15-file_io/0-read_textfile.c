4098a7932919f21172a46ec2f53f8adac1bdf9c3
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-25 13:54:49 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,33 @@
 #include "main.h"
 /**
- * read_textfile - for reading text file from standard input
- * @letters: the number of letters to red
- * Return: Always 0
+ * read_textfile - for reading to posix
+ * @filename: name of the file 
+ * @letters: number of character to be read
+ * Return: letter if success or 0 if failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *point;
-	point = read(STDIN_FILENO, filename, letters);
-	if (point == NULL)
-		return (0);
+	ssize_t op, rd, wr;
+	char *charnum;
+
 	if (filename == NULL)
 		return (0);
-	if 
+
+	charnum = malloc(sizeof(char) * letters);
+	if (charnum == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, charnum, letters);
+	wr = write(STDOUT_FILENO, charnum, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(charnum);
+		return (0);
+	}
+
+	free(charnum);
+	close(op);
+	return (wr);
+}
