f7c77f1c064c81e4d1aa6062014a5a997a1b0ed1
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-25 15:29:42 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * print-binary - for printing binary
+ * print_binary - for printing binary
  * @n: the number to be converted
  * Return: Alwayd 0
  */
 void print_binary(unsigned long int n)
 {
+	if (n > 1)
+		print_binary(n >> 1);
 
+	_putchar((n & 1) + '0');
 }
