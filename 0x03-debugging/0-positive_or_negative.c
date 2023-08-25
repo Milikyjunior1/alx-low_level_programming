5dfe0b1ffbf58e19aeb584402c0b8023818c2ac4
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-25 16:09:45 -0700

    0-positive_or_negative.c

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -7,17 +7,17 @@
 */
 int main(void)
 {
-	int n;
+	int d;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	d = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (d > 0)
+		printf("%d is positive\n", d);
+	else if (d < 0)
+		printf("%d is negative\n", d);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", d);
 
 	return (0);
 }
