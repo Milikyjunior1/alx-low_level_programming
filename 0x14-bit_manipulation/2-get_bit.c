4bb9cbcca9d4ab1bd2afa3200ad33beb41a698d9
Milikyjunior1 <awopetuolamilekan2023@gmail.com>
2023-08-31 20:10:06 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int *conv, **arr, ind, suc;
+	unsigned int *conv, **arr, ind;
 
 	while (n && index)
 	{
@@ -17,13 +17,17 @@ int get_bit(unsigned long int n, unsigned int index)
 		{
 			*conv = (*conv * 10) + (n % 2);
 			arr = &conv;
-			for (ind = 0; ind < index; ind++)
+			for (ind = 0; ind <= index; ind++)
 			{
 			}
-			return (*arr[ind]);
-			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-	return (-1);
+	if (*arr[ind] == 1)
+	{
+		return (1);
+	}
+	else if (*arr[ind] == 0)
+		return (0);
+	else
+		return (-1);
 }
